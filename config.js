d6f19e9f0a33b31f85136dbcf08b85f075fabde8
emmanuelkine <emmanuelkine@gmail.com>
2026-07-28 11:54:51 -0400

    Configura Supabase y curso protegido

---
+++ config.js
@@ -0,0 +1,1 @@
+window.KINECHECK_CONFIG=Object.freeze({supabaseUrl:'https://eqhcdclyeoapmqtlduwf.supabase.co',supabaseAnonKey:'sb_publishable_FTwhDZYCF3zf7W9rB7bFwQ_rF9Y7OX_',courseSlug:'evidencia-aplicada',courseKeyFunction:'course-key',progressTable:'learning_progress',supportEmail:'emmanuelkine@gmail.com'});
